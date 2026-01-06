78134dce1239b3b05b7f0f1aa56958840f211439
Acryt <acryter@gmail.com>
2026-01-06 15:45:52 +0300

    definition, declaration

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,17 +1,14 @@
 local function setup_keymaps()
-	-- Leader key
-	vim.g.mapleader = " "
-	vim.g.maplocalleader = "\\"
-	-- Neotree toggle
 	vim.keymap.set("n", "<leader>e", ":Neotree toggle reveal<CR>", {
 		noremap = true,
 		silent = true,
 		desc = "Toggle Neotree",
 	})
-	-- lazy
 	vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 	vim.keymap.set("n", "<leader>m", "<cmd>Mason<cr>", { desc = "Mason" })
 	vim.keymap.set("n", "<leader>g", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
+	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 end
 
 -- Вызов функции настройки
